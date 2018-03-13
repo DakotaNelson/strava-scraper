c2b4ce4d97191411b07515bafb7c3587ad8cab73
Dakota Nelson <dakota.w.nelson@gmail.com>
2018-03-12 22:12:16 -0700

    initial commit, can scrape tiles and save to fs

---
+++ README.md
@@ -0,0 +1,11 @@
+# strava-scraper
+
+It scrapes Strava.
+
+To set up:
+
+`virtualenv --python=python3 venv && pip install requirements.txt`
+
+To run:
+
+`source venv/bin/activate && cd tile_scraper && scrapy crawl tile_scraper`
---
+++ tile_scraper/tile_scraper/spiders/example.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import math
+
+import scrapy
+
+class ExampleSpider(scrapy.Spider):
+    name = 'tile_scraper'
+
+    allowed_domains = [
+            'heatmap-external-a.strava.com',
+            'heatmap-external-b.strava.com',
+            'heatmap-external-c.strava.com'
+    ]
+
+    def start_requests(self):
+        """ calculate out all the tiles we need to get, then yield each tile's
+        URL for the spider to grab """
+        max_zoom = 1
+        for zoom in range(0,max_zoom+1):
+            tile_extent = 2 ** zoom
+            for x in range(tile_extent):
+                for y in range(tile_extent):
+                    url = 'http://heatmap-external-a.strava.com/tiles/all/hot/{zoom}/{x}/{y}.png'.format(
+                            zoom=zoom,
+                            x=x,
+                            y=y
+                    )
+                    yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        # take the ".png" off,
+        # then split by "/",
+        # then take only the last 3 elements
+        zoom, x, y = response.url[:-4].split("/")[-3:]
+        filename = "{zoom}.{x}.{y}.png".format(
+                zoom=zoom,
+                x=x,
+                y=y
+        )
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log("Saved {}".format(filename))
